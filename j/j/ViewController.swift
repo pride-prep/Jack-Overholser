fe9f44b5eca9180e6a02a53c16ee9f4675b67f21
jack <jack@my.prideprepschool.org>
2017-05-17 13:14:19 -0700

    Add files via upload

--- j/j/ViewController.swift
+++ j/j/ViewController.swift
@@ -11,13 +11,49 @@ import UIKit
 class ViewController: UIViewController {
     
     var numberOnScreen:Double = 0;
+    var previousNumber:Double = 0;
+    var performingMath = false
     
     @IBOutlet weak var label: UILabel!
     
     @IBAction func numbers(_ sender: UIButton) {
-        label.text = label.text! + String(sender.tag-1)
-        numberOnScreen = Double(label.text!)!
+        
+        if performingMath == true {
+            label.text = String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+            performingMath = false
+            
+        }
+        else
+        {
+            label.text = label.text! + String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+        }
+        
+    
     }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if label.text != "" && sender.tag != 11 && sender.tag != 16 {
+            
+            previousNumber = Double(label.text!)!
+            if sender.tag == 12 { //Divide
+                
+            }
+            else if sender.tag == 13 { //Multiply
+                
+            }
+            else if sender.tag == 14 { //Minus
+                
+            }
+            else if sender.tag == 15 { //Plus
+                label.text = "+";
+            }
+            
+            performingMath = true;
+        }
+    }
+    
     
 
     override func viewDidLoad() {
